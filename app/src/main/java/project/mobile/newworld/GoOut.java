1f34d06aa6559c25a80a68de015201718e53a54a
jdurbanek <jdurbanek@wisc.edu>
2016-04-03 17:52:59 -0500

    Added button transitions between activities and fragments. Still need to add home button actions.

--- app/src/main/java/project/mobile/newworld/GoOut.java
+++ app/src/main/java/project/mobile/newworld/GoOut.java
@@ -1,7 +1,9 @@
 package project.mobile.newworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GoOut extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class GoOut extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_out);
+
+
+    }
+
+    public void launchCurrentWeek(View view){
+        Intent intent = new Intent(this, MyWeek.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/project/mobile/newworld/MyWeek.java
@@ -0,0 +1,19 @@
+package project.mobile.newworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyWeek extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_week);
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, CurrentWeek.newInstance(null, null))
+                .addToBackStack(null)
+                .commit();
+    }
+}
