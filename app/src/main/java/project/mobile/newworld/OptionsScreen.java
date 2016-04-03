fe7962b2ee3fcde30e15e9dfa6272ad51e4300f1
jdurbanek <jdurbanek@wisc.edu>
2016-04-03 17:13:23 -0500

    Added Activities for the three main functions and transitions to them.

---
+++ app/src/main/java/project/mobile/newworld/OptionsScreen.java
@@ -0,0 +1,30 @@
+package project.mobile.newworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class OptionsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options_screen);
+    }
+
+    public void base(View view){
+        Intent intent = new Intent(this, Base.class);
+        startActivity(intent);
+    }
+
+    public void out(View view){
+        Intent intent = new Intent(this, GoOut.class);
+        startActivity(intent);
+    }
+
+    public void battle(View view){
+        Intent intent = new Intent(this, Battle.class);
+        startActivity(intent);
+    }
+}
