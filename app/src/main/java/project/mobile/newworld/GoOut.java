76406287d94857cd2c7416492758be2d04392603
jdurbanek <jdurbanek@wisc.edu>
2016-04-13 16:55:24 -0500

    added map frag.

--- app/src/main/java/project/mobile/newworld/GoOut.java
+++ app/src/main/java/project/mobile/newworld/GoOut.java
@@ -1,6 +1,11 @@
 package project.mobile.newworld;
 
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +17,11 @@ public class GoOut extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_out);
 
+        MapFragment frg = new MapFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.map_frag_container, frg, "Map");
+        transaction.commit();
 
     }
 
