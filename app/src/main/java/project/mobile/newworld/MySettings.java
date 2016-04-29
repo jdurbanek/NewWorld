28db681fa55f6703af1087f0af8d150a26f1fda0
jdurbanek <jdurbanek@wisc.edu>
2016-04-28 19:21:12 -0500

    Added settings option to set home wifi.

--- app/src/main/java/project/mobile/newworld/MySettings.java
+++ app/src/main/java/project/mobile/newworld/MySettings.java
@@ -1,9 +1,13 @@
 package project.mobile.newworld;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,7 +22,10 @@ public class MySettings extends AppCompatActivity {
     private EditText name;
     private EditText sGoals;
     private TextView goalLabel;
+    private TextView currentHome;
+    private Button updateWifi;
     private int myGoal = 10000;
+    private String setHome = "Not set";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +37,15 @@ public class MySettings extends AppCompatActivity {
         name = (EditText)findViewById(R.id.nameEText);
         sGoals = (EditText)findViewById(R.id.stepGoals);
         goalLabel = (TextView)findViewById(R.id.goalsLabel);
+        currentHome = (TextView)findViewById(R.id.currentHome);
+        updateWifi = (Button)findViewById(R.id.updateHome);
 
         SharedPreferences settings = getSharedPreferences("BaseInfo", 0);
         boolean amMale = settings.getBoolean("Male", false);
         boolean amFemale = settings.getBoolean("Female", false);
         String myName = settings.getString("Name", "User");
         int stepG = settings.getInt("Goal", 10000);
+        String myWifi = settings.getString("Wifi", setHome);
 
         male.setChecked(amMale);
         female.setChecked(amFemale);
@@ -47,6 +57,7 @@ public class MySettings extends AppCompatActivity {
         name.setText(myName);
         goalLabel.setText("Step Goal: " + stepG);
         myGoal = stepG;
+        currentHome.setText("Current Home: " +  myWifi);
     }
 
     public void saveSettings(View view){
@@ -69,6 +80,13 @@ public class MySettings extends AppCompatActivity {
         finish();
     }
 
+    public void updateMyWifi(View view){
+        WifiManager wifiManager =
+                (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifi = wifiManager.getConnectionInfo();
+        setHome = wifi.getSSID();
+    }
+
     @Override
     protected void onStop(){
         super.onStop();
@@ -86,6 +104,7 @@ public class MySettings extends AppCompatActivity {
         editor.putString("Name", name.getText().toString().trim());
         editor.putBoolean("Male", male.isChecked());
         editor.putBoolean("Female", female.isChecked());
+        editor.putString("Wifi", setHome);
         editor.commit();
     }
 }
