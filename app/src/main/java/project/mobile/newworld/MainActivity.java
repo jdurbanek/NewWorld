1c62bea71c0e49011f61311261e41f0d94307916
jdurbanek <jdurbanek@wisc.edu>
2016-05-01 15:34:38 -0500

    Removed some toasts and added all imagebuttons to base activity

--- app/src/main/java/project/mobile/newworld/MainActivity.java
+++ app/src/main/java/project/mobile/newworld/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences settings = getSharedPreferences("BaseInfo", 0);
         int baseLevel = settings.getInt("Base Level", 1);
         String mac = settings.getString("MAC", "");
-        Toast toast = Toast.makeText(getApplicationContext(), mac, Toast.LENGTH_LONG);
-        toast.show();
+      //  Toast toast = Toast.makeText(getApplicationContext(), mac, Toast.LENGTH_LONG);
+       // toast.show();
         BroadcastReceiver br = new WifiBroadcastReceiver(getApplicationContext(), mac);
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
